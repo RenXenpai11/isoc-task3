ba3a6bc2bead0f17845df49f066f261995ca0234
Rechiel Mae Inderio <r.inderio.533722@umindanao.edu.ph>
2026-03-12 14:55:52 +0800

    third commit

--- Project.js
+++ Project.js
@@ -1,9 +1,20 @@
-
-class Project {
-    constructor(name, description, startDate, endDateee) {
-        this.name = name;
-        this.description = description;
-        this.startDate = startDate;
-        this.endDate = endDateee;
-    }
+function getBudget() {
+  try {
+    // Get the budget value from Finance sheet, cell B19
+    const result = getCellValue('Finance', 'B19');
+    
+    // Log the result
+    console.log(result);
+    
+    // Return the result
+    return result;
+    
+  } catch (error) {
+    const errorResult = {
+      success: false,
+      error: error.message
+    };
+    console.log(errorResult);
+    return errorResult;
+  }
 }
