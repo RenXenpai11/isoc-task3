5a730329812f9c8b9cb16e109404db6396cbc412
Rechiel Mae Inderio <r.inderio.533722@umindanao.edu.ph>
2026-03-13 15:44:43 +0800

    commit new

--- Helper Functions.js
+++ Helper Functions.js
@@ -52,3 +52,27 @@ function getCellValue(sheetName, cellRange) {
   }
 }
 
+function getProjectSummaryPayload() {
+  return {
+    success: true,
+    data: {
+      total_projects: 1,
+      ontrack_projects: 1,
+      atrisk_projects: 0,
+      delayed_projects: 0
+    }
+  };
+}
+
+function doGet() {
+  const payload = getProjectSummaryPayload();
+  return ContentService.createTextOutput(JSON.stringify(payload))
+    .setMimeType(ContentService.MimeType.JSON);
+}
+
+function testProjectSummaryPayload() {
+  const payload = getProjectSummaryPayload();
+  Logger.log(JSON.stringify(payload, null, 2));
+  return payload;
+}
+
